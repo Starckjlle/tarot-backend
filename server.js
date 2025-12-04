f067808fcc3c2472f05f22d2180e14822dbd67ef
Ricardo <ricardo@MacBook-Air-Ricardo.local>
2025-12-03 21:45:38 -0300

    Muda porta local para 4000

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 
 // Porta obrigatória: a Render injeta process.env.PORT
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
